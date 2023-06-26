f34d2f2331ef65033d439a8bea3394982aceac5c
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-06-26 21:18:36 +0200

    Add weather icons

--- src/components/MainTemperatureCard.tsx
+++ src/components/MainTemperatureCard.tsx
@@ -1,19 +1,45 @@
-// import clearDay from '../assets/img/clear-day.svg';
+// import clear from '../assets/img/clear-day.svg';
+// import cloudy from '../assets/img/cloudy.svg';
+
+// import { useState } from 'react';
+
 // import bgImage from 'https://cdn.pixabay.com/photo/2016/01/02/01/51/clouds-1117584_1280.jpg';
 
 const MainTemperatureCard = (props: any) => {
+    // const weatherStatus = props.weather.weather[0].main;
+    const weatherStatus = props.weather.weather[0].description;
+    // const [img, setImg] = useState('');
+
+    // switch (weatherStatus) {
+    //     case 'Clear':
+    //         setImg(clear);
+    //         break;
+    //     case 'Clouds':
+    //         setImg(cloudy);
+    //         break;
+    //     default:
+    //         setImg('');
+    // }
     return (
         <div className="temperature-card">
-            {/* <img src={} alt="weather image" /> */}
+            {/* {img ? <img src={img} alt={weatherStatus + ' image'} /> : null} */}
+            {/* <img src={img} alt={weatherStatus + ' image'} /> */}
+            <img
+                src={`https://openweathermap.org/img/wn/${props.weather.weather[0].icon}@2x.png`}
+                alt={weatherStatus}
+            />
 
             <h2>{props.weather.name}</h2>
+            {/* Staronizhestebliyevskaya */}
 
             <div className="temperature">
                 {Math.round(props.weather.main.temp)}
                 <sup>°C</sup>
             </div>
 
-            <div className="weather-status">{props.weather.weather[0].main}</div>
+            <div className="weather-status">
+                {weatherStatus.charAt(0).toUpperCase() + weatherStatus.slice(1)}
+            </div>
 
             <ul>
                 <li>
